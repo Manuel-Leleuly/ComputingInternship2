d8597816df4542d8ac5a7b7261e8537a22774d95
manuel-leleuly <manuel.leleuly@gmail.com>
2019-02-25 23:38:33 +0700

    Perbaikan Kodingan Pets.py

--- Pets.py
+++ Pets.py
@@ -1,40 +1,45 @@
 class Pets:
     pet_list = []
-    def __init__(self,pet_name,pet_age,pet_hungry):
+    def __init__(self):
         animal = []
-        animal.append(pet_name)
-        animal.append(pet_age)
-        animal.append(pet_hungry)
-        pet_list.append(animal)
+        animal.append(self.pet_name)
+        animal.append(self.pet_age)
+        animal.append(self.pet_hungry)
+        self.pet_list.append(animal)
     def is_hungry():
         for i in range(len(pet_list)):
             if pet_list[i][2] == True:
                 whos_hungry(pet_list[i])
     def whos_hungry(pet):
-        print(pet_list[0])
+        print(pet[0])
 
-class Animal:
+class Animal():
     def __init__(self,pet_name,pet_age):
         self.pet_name = pet_name
         self.pet_age = pet_age
         self.pet_hungry = True
+        Pets.__init__(self)
     def eat(self):
         self.pet_hungry = False
 
-class cat:
-    species = 'Felis Cailus'
-    kucing1 = Animal('bambang',10)
+class cat(Animal):
+    species = 'Felis Catus'
+    def __init__(self, pet_name, pet_age):
+        Animal.__init__(self, pet_name, pet_age)
     def sound_kucing():
         print('Miaw')
 
-class dog:
-    species = 'Anjing'
-    anjing1 = Animal('Budi',12)
+class dog(Animal):
+    species = 'Anjingus Gukgukus'
+    def __init__(self, pet_name, pet_age):
+        Animal.__init__(self, pet_name, pet_age)
     def sound_anjing():
         print('Guk')
 
-class bird:
-    species = 'burung'
+class bird(Animal):
+    species = 'Burungus Meong'
+    def __init__(self, pet_name, pet_age):
+        Animal.__init__(self, pet_name, pet_age)
     def sound_burung():
         print('cirp')
     
